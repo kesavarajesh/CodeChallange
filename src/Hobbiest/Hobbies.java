692757da7059cb40b4ec9f0819c73fd87c7faa5d
kesavarajesh <kesava.rajesh@gmail.com>
2021-08-18 12:30:19 -0500

    Code Challange 1

---
+++ src/Hobbiest/Hobbies.java
@@ -0,0 +1,40 @@
+package Hobbiest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import java.util.*;
+
+public class Hobbies {
+	   
+    private final HashMap<String, String[]> hobbies = new HashMap<String, String[]>();
+   
+    public void add(String hobbyist, String... hobbies) {
+        this.hobbies.put(hobbyist, hobbies);
+    }
+   
+    public List<String> findAllHobbyists(String hobby) {
+    List<String> list=new ArrayList<>();
+   
+    this.hobbies.entrySet().stream().forEach(e -> {
+    for (String s : e.getValue())
+    {
+    if (s.equals(hobby))
+    {
+    list.add(e.getKey());
+    }
+    }
+        });
+    return list;
+    }
+   
+    public static void main(String[] args) {
+        Hobbies hobbies = new Hobbies();
+        hobbies.add("Steve", "Fashion", "Piano", "Reading");
+        hobbies.add("Patty", "Drama", "Magic", "Pets");
+        hobbies.add("Chad", "Puzzles", "Pets", "Yoga");
+
+        System.out.println(Arrays.toString(hobbies.findAllHobbyists("Yoga").toArray()));
+    }
+}
